5100640fb9a04e89ebabe310020910f1fe6aebc2
lucas <lucasa.soaresmar@gmail.com>
2018-09-02 21:08:35 -0300

    Initial commit

---
+++ src/App.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { shallow } from 'enzyme';
+
+it('renders without crashing', () => {
+	const wrapper = shallow(<App/>);
+	const welcome = <h1 className="App-title">Welcome to React</h1>;
+	expect(wrapper.contains(welcome)).toEqual(true);
+});

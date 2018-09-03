f1ba0151ab4ec25464a805fe499318961ffc5253
lucas <lucasa.soaresmar@gmail.com>
2018-09-03 20:48:14 -0300

    Add exchanges to localStorage DB

--- src/DB/dataBase.js
+++ src/DB/dataBase.js
@@ -8,10 +8,32 @@ export const DB = {
 				password: '123',
 		 		real: 100000,
 		 		brita: 0,
-		 		bitcoin: 0
+		 		bitcoin: 0,
+		 		exchanges: [1,2]
 			}
 		},
-		usersId: [1]
+		usersId: [1],
+		exchanges: {
+			1: {
+				id: 1,
+				userId: 1,
+				date: '21/08/2018',
+				from: 'brita',
+				to: 'real',
+				amount: 200.22,
+				conversion: 13
+			},
+			2: {
+				id: 2,
+				userId: 1,
+				date: '21/08/2018',
+				from: 'real',
+				to: 'bitcoin',
+				amount: 300.98,
+				conversion: 0.16
+			}
+		},
+		exchangesId: [1,2]
 	},
 	findUser: function(user) {
 		let userId;
@@ -36,23 +58,58 @@ export const DB = {
 	 			password: newUser.password,
 				real: 100000,
 	 			brita: 0,
-	 			bitcoin: 0
+	 			bitcoin: 0,
+	 			exchanges: []
 			}
 		}
 
 		this.storage.usersId = [...this.storage.usersId, userId]
-		this.saveToStorage();
+		this.saveToStorage()
 		
 		return { status: 201, data: this.storage.users[userId] }
 	},
 	authUser: function(authUser) {
 		const savedUser = this.findUser(authUser)
 		
-		if (!savedUser) return { status: 400, data: 'Cadastre-se primeiro :)'}
+		if (!savedUser) return { status: 400, data: 'Cadastre-se primeiro :)' }
 
 		return savedUser.password === authUser.password
 			? { status: 200, data: savedUser }
 			: { status: 401, data: 'Senha errada :(' }
+	},
+	findExchanges: function(user) {
+		const savedUser = this.findUser(user)
+		
+		if (!savedUser) return { status: 400, data: 'Cadastre-se antes de comprar :)' }
+
+		const exchanges = savedUser.exchanges.map(exchange => this.storage.exchanges[exchange])
+
+		return { status: 200, data: exchanges }
+	},
+	addExchange: function(exchange) {
+		const user = this.findUser(this.storage.users[exchange.userId]);
+
+		if (!user) return {status: 404, data: 'Usuário não encontrado'}
+
+		const exchangeId = this.storage.exchangesId.length + 1
+
+		this.storage.exchanges = {
+			...this.storage.exchanges,
+			[exchangeId]: {
+				id: exchange.exchangeId,
+				userId: exchange.userId,
+				date: exchange.date,
+				from: exchange.from,
+				to: exchange.to,
+				amount: exchange.amount,
+				conversion: exchange.conversion
+			}
+		}
+
+		this.storage.exchangesId = [...this.storage.exchangesId, exchangeId]
+		this.saveToStorage()
+
+		return { status: 201, data: this.storage.exchanges[exchangeId] }
 	},
 	saveToStorage: function() {
 		localStorage.setItem(this.storageName, JSON.stringify(this.storage))

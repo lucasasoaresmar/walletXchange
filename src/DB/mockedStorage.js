837fba88ce5b7d4b7a0a6443975bd9cf33d0d5b9
lucas <lucasa.soaresmar@gmail.com>
2018-09-03 22:37:02 -0300

    Add mockedStorage

---
+++ src/DB/mockedStorage.js
@@ -0,0 +1,35 @@
+export const mockedStorage = {
+	users: {
+		1: {
+			id: 1,
+			name: 'Lucas',
+			password: '123',
+			real: 100000,
+			brita: 0,
+			bitcoin: 0,
+			exchanges: [1,2]
+		}
+	},
+	usersId: [1],
+	exchanges: {
+		1: {
+			id: 1,
+			userId: 1,
+			date: '21/08/2018',
+			from: 'brita',
+			to: 'real',
+			amount: 200.22,
+			conversion: 13
+		},
+		2: {
+			id: 2,
+			userId: 1,
+			date: '21/08/2018',
+			from: 'real',
+			to: 'bitcoin',
+			amount: 300.98,
+			conversion: 0.16
+		}
+	},
+	exchangesId: [1,2]
+}

c7fbeac898f24bdd8eaac462ca975a361515fdee
lucas <lucasa.soaresmar@gmail.com>
2018-09-08 18:16:19 -0300

    Add redux reducers

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './components/App'
+import registerServiceWorker from './registerServiceWorker'
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import reducer from './reducers'
+import { DB } from './DB/dataBase.js'
 
-import { DB } from './DB/dataBase.js';
+const store = createStore(
+  reducer,
+  applyMiddleware(thunk)
+)
 
 DB.loadStorage()
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+	<Provider store={ store } >
+		<App />
+	</Provider>,
+	document.getElementById('root'))
+registerServiceWorker()
---
+++ src/reducers/index.js
@@ -0,0 +1,73 @@
+import { combineReducers } from 'redux'
+import {
+	TOGGLE_USER_REQUEST,
+	RECEIVE_USER,
+	USER_FAILURE,
+	USER_LOGOUT,
+	TOGGLE_MONEY_REQUEST,
+	RECEIVE_MY_MONEY,
+	MONEY_FAILURE,
+	RECEIVE_EXCHANGES
+} from '../constants/ActionTypes'
+
+const userState = {
+	requiringUser: false,
+	requiringMoney: false
+}
+
+const user = (state = userState, action) => {
+	switch(action.type) {
+		case TOGGLE_USER_REQUEST:
+			return {
+				...state,
+				requiringUser: !state.requiringUser
+			}
+		case RECEIVE_USER:
+			return {
+				...state,
+				...action.user
+			}
+		case USER_FAILURE:
+			return {
+				...state,
+				userFailure: action.message
+			}
+		case USER_LOGOUT:
+			return {}
+		case TOGGLE_MONEY_REQUEST: 
+			return {
+				...state,
+				requiringMoney: !state.requiringMoney
+			}
+		case RECEIVE_MY_MONEY:
+			return {
+				...state,
+				...action.money
+			}
+			case MONEY_FAILURE: {
+				return {
+					...state,
+					moneyFailure: action.message
+				}
+			}
+		default:
+			return state
+	}
+}
+
+const exchanges = (state = [], action) => {
+	switch (action.type) {
+		case RECEIVE_EXCHANGES:
+			return [
+				...state,
+				...action.exchanges
+			]
+		default:
+			return state
+	}
+}
+
+export default combineReducers({
+	user,
+	exchanges
+})

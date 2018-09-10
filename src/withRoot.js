3ed298b6575f2b53ae4b7fec504dde7424d14067
lucas <lucasa.soaresmar@gmail.com>
2018-09-09 23:49:14 -0300

    Implement material UI theme provider

---
+++ src/withRoot.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import purple from '@material-ui/core/colors/purple';
+import green from '@material-ui/core/colors/green';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: purple[300],
+      main: purple[500],
+      dark: purple[700],
+    },
+    secondary: {
+      light: green[300],
+      main: green[500],
+      dark: green[700],
+    },
+  },
+});
+
+function withRoot(Component) {
+  function WithRoot(props) {
+    return (
+      <MuiThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...props} />
+      </MuiThemeProvider>
+    );
+  }
+
+  return WithRoot;
+}
+
+export default withRoot;

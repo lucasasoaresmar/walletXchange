098c4a6740c39e0dc29a1b3f12bd0ea480e6cd3c
lucas <lucasa.soaresmar@gmail.com>
2018-09-12 22:05:56 -0300

    Implement Material UI in MoneyPanel

--- src/containers/MoneyPanel.container.js
+++ src/containers/MoneyPanel.container.js
@@ -1,10 +1,11 @@
 import { connect } from 'react-redux'
 import MoneyPanel from '../components/MoneyPanel'
 
-const mapStateToProps = (state) => ({
+const mapStateToProps = (state, ownProps) => ({
 	real: state.user.real,
 	brita: state.user.brita,
-	bitcoin: state.user.bitcoin
+	bitcoin: state.user.bitcoin,
+	classes: ownProps.classes
 })
 
 export default connect (
